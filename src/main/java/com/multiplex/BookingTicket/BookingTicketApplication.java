3abd3c5a23c58bb3595d9eb8a2a59e0cdf0a4bc8
akshay kumar <akshaykv1tbm@gmail.com>
2018-08-30 09:59:53 +0530

    .

--- src/main/java/com/multiplex/BookingTicket/BookingTicketApplication.java
+++ src/main/java/com/multiplex/BookingTicket/BookingTicketApplication.java
@@ -3,10 +3,10 @@ package com.multiplex.BookingTicket;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@DataMongoTest
+
 @EnableMongoRepositories
 @SpringBootApplication
 public class BookingTicketApplication {
